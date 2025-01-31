ce8e18c4db389a7f035b6f19d83b974900341bf7
farza <farzanrichu610@gmai.com>
2025-01-31 21:04:57 +0530

    ..

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-cursor= document.querySelector(".moveCurs")
-document.addEventListener('mousemove', function(e){
-    let x=e.clientX;
-    let y=e.clientY;
+cursor = document.querySelector(".moveCurs")
+document.addEventListener('mousemove', function (e) {
+    let x = e.clientX;
+    let y = e.clientY;
     cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
 });
+
+function toggleMenu(x) {
+    x.classList.toggle('change');
+
+}
+
